fc041a057cba33fa3166214eed57170e9c9e464b
Huy Nguyen <hnguyen10082000@gmail.com>
2024-01-09 13:55:19 -0500

    added validation

--- process.php
+++ process.php
@@ -11,21 +11,61 @@
     <h1>Thank you for your order</h1>
 
     <?php
+    //Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    //Define constants
+    define('PRICE_PER_SCOOP', 2.50);
+    define('TAX', .11);
+
     echo "<pre>";
     var_dump($_POST);
     echo "</pre>";
 
-    $scoops = $_POST['scoops'];
-    $flavors = $_POST['flavor'];
-    $cone = $_POST['cone'];
+    //scoops validation
+    if (!empty($_POST['scoops'])){
+        $scoops = $_POST['scoops'];
+    }
+    else {
+        echo "<p> Enter scoops! </p>";
+        return;
+    }
+
+    //flavor validation
+    if (isset($_POST['flavor'])){
+        $flavors = $_POST['flavor'];
+    }
+    else{
+        echo "<p> Please select at least one flavor </p>";
+        return;
+    }
+
+    //cones validation
+    if (!empty($_POST['cone'])){
+        $cones = $_POST['cone'];
+    }
+    else {
+        echo "<p> Enter cones! </p>";
+        return;
+    }
+
     $flavorString = implode(", ", $flavors);
 
     //print a summary
     echo "<p>$scoops scoops</p>";
     echo "<p> Flavor: $flavorString  </p>";
     echo "<p>$scoops scoops</p>";
-    echo "<p>Cone: $cone </p>";
-    ?>
+    echo "<p>Cone: $cones </p>";
 
+    $subtotal = PRICE_PER_SCOOP * $scoops;
+    $tax = $subtotal * TAX;
+    $total = $subtotal + $tax;
+
+    echo "<p> subtotal: $subtotal </p>";
+    echo "<p> tax: $tax </p>";
+    echo "<p> total: $total </p>";
+
+    ?>
 </body>
 </html>
